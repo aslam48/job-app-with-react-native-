80cffc5310ec24f1d9f1fa39def46f5f41b2cfb5
aslam48 <youngfizzy001@gmail.com>
2023-05-28 13:03:26 +0100

    fixing api

---
+++ hook/useFetch.js
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { RAPID_API_KEY } from "@env";
+
+const rapidapiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "69a5930a6cmsh5204b887746d7c6p1f08a2jsne0cece879d15",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      // alert("there is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchData()
+  },[]);
+
+  const refetch = () => {
+    setIsLoading(true)
+    fetchData()
+  }
+
+  return  {data, isLoading, error, refetch}
+};
+export default useFetch
+
+
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// // import { RAPID_API_KEY } from "@env";
+
+// // const rapidapiKey = RAPID_API_KEY;
+
+// const useFetch = (endpoint, query) => {
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   const options = {
+//     method: "GET",
+//     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+//     headers: {
+//       "X-RapidAPI-Key": '69a5930a6cmsh5204b887746d7c6p1f08a2jsne0cece879d15',
+//       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+//     },
+//     params: { ...query },
+//   };
+
+//   const fetchData = async () => {
+//     setIsLoading(true);
+
+//     try {
+//       const response = await axios.request(options);
+
+//       setData(response.data.data);
+//       setIsLoading(false);
+//     } catch (error) {
+//       setError(error);
+//       console.log(error)
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   const refetch = () => {
+//     setIsLoading(true);
+//     fetchData();
+//   };
+
+//   return { data, isLoading, error, refetch };
+// };
+
+// export default useFetch;
